f65d74c3894e5a1a499413cb5d0e45fa7de30c5c
FreddyFiend <frddbrn@gmail.com>
2023-04-09 12:39:36 +0500

    Added navbar

--- frontend/src/layouts/Navbar.tsx
+++ frontend/src/layouts/Navbar.tsx
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { UserContext } from "../providers/UserProvider";
-import {} from "react-icons";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 const Navbar = () => {
   const { user, setUser } = useContext(UserContext);
@@ -13,75 +13,49 @@ const Navbar = () => {
     localStorage.removeItem("user");
   };
   return (
-    <div className="flex justify-between h-24">
-      <div className="  text-2xl flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-        King
-      </div>
-      <ul className="hidden sm:block p-2 list-none flex gap-2">
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="signup">
-            <button className="s-btn text-sm">Products</button>
-          </Link>
-        </li>
-        <li>
-          <Link to="signup">
-            <button className="s-btn text-sm">Cart</button>
-          </Link>
-        </li>
-        <li>
-          <Link to="signup">
-            <button className="s-btn text-sm">Signup</button>
-          </Link>
-        </li>
-        <li>
-          <Link to="login">
-            <button className="p-btn text-sm">Login</button>
-          </Link>
-        </li>
-      </ul>
-      <button className=" sm:hidden " onClick={toggleNavbar}>
-        {" "}
-        {isOpen ? (
-          <svg
-            className="block h-6 w-6"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            aria-hidden="true"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M6 18L18 6M6 6l12 12"
-            />
-          </svg>
-        ) : (
-          <svg
-            className="block h-6 w-6"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            aria-hidden="true"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M4 6h16M4 12h16M4 18h16"
-            />
-          </svg>
-        )}
-      </button>
+    <div>
+      <div className="flex justify-between h-10 ">
+        <div className="text-2xl flex flex-1 justify-center sm:items-stretch sm:justify-start">
+          King
+        </div>
+
+        <ul className="hidden sm:flex p-2 list-none gap-2">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="signup">
+              <button className="s-btn text-sm">Products</button>
+            </Link>
+          </li>
+          <li>
+            <Link to="signup">
+              <button className="s-btn text-sm">Cart</button>
+            </Link>
+          </li>
+          <li>
+            <Link to="signup">
+              <button className="s-btn text-sm">Signup</button>
+            </Link>
+          </li>
+          <li>
+            <Link to="login">
+              <button className="p-btn text-sm">Login</button>
+            </Link>
+          </li>
+        </ul>
+        <button
+          className=" sm:hidden absolute right-1 top-2"
+          onClick={toggleNavbar}
+        >
+          {" "}
+          {isOpen ? <AiOutlineClose size={24} /> : <AiOutlineMenu size={24} />}
+        </button>
+      </div>{" "}
       <ul
         className={`sm:hidden  ${
           isOpen ? "block" : "hidden"
-        }  p-2 list-none flex-col gap-2`}
+        }  p-2 list-none flex-col gap-2 `}
       >
         <li>
           <Link to="/">Home</Link>
